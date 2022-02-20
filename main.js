405362714958aaaa81906c13c8a06fbf957e9b71
Thomas <venturini.thomas95@gmail.com>
2022-02-21 00:26:38 +0100

    esercizio finito più bonus

---
+++ main.js
@@ -0,0 +1,20 @@
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            emails : [],
+        },
+
+        mounted() {
+
+            //qua mettero api con generatore email
+            for ( let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    this.emails.push(response.data.response);
+                    
+                    })
+                };
+        }
+    }
+);
